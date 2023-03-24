cdb99f731bb388fda4d9ded980363d853cb95c68
victor <vici8910@gmail.com>
2023-03-24 22:44:56 +0100

    typed manager load

--- Manager/MenuManager.php
+++ Manager/MenuManager.php
@@ -13,199 +13,186 @@ use Prodigious\Sonata\MenuBundle\Repository\MenuitemRepository;
  */
 class MenuManager
 {
-    const STATUS_ENABLED = true;
-    const STATUS_DISABLED = false;
-    const STATUS_ALL = null;
+	const STATUS_ENABLED = TRUE;
+	const STATUS_DISABLED = FALSE;
+	const STATUS_ALL = NULL;
 
-    const ITEM_ROOT = true;
-    const ITEM_CHILD = false;
-    const ITEM_ALL = null;
+	const ITEM_ROOT = TRUE;
+	const ITEM_CHILD = FALSE;
+	const ITEM_ALL = NULL;
 
-    /**
-     *
-     * @var EntityManagerInterface
-     */
-    protected $em;
+	/**
+	 * @var EntityManagerInterface
+	 */
+	protected $em;
 
-    /**
-     * @var MenuRepository
-     */
-    protected $menuRepository;
+	/**
+	 * @var MenuRepository
+	 */
+	protected $menuRepository;
 
-    /**
-     * @var MenuItemRepository
-     */
-    protected $menuItemRepository;
+	/**
+	 * @var MenuItemRepository
+	 */
+	protected $menuItemRepository;
 
-    /**
-     * Constructor
-     *
-     * @param EntityManagerInterface $em
-     */
-    public function __construct(EntityManagerInterface $em)
-    {
-        $this->em = $em;
-        $this->menuRepository = $em->getRepository(MenuInterface::class);
-        $this->menuItemRepository = $em->getRepository(MenuItemInterface::class);
-    }
+	/**
+	 * Constructor
+	 * @param EntityManagerInterface $em
+	 */
+	public function __construct(EntityManagerInterface $em)
+	{
+		$this->em = $em;
+		$this->menuRepository = $em->getRepository(MenuInterface::class);
+		$this->menuItemRepository = $em->getRepository(MenuItemInterface::class);
+	}
 
-    /**
-     * Load menu by id
-     *
-     * @param int $id
-     * @return Menu
-     */
-    public function load($id)
-    {
-        $menu = $this->menuRepository->find($id);
+	/**
+	 * Load menu by id
+	 * @param int $id
+	 * @return Menu
+	 */
+	public function load($id): MenuInterface
+	{
+		$menu = $this->menuRepository->find($id);
 
-        return $menu;
-    }
+		return $menu;
+	}
 
-    /**
-     * Load menu by alias
-     *
-     * @param string $alias
-     * @return Menu
-     */
-    public function loadByAlias($alias)
-    {
-        $menu = $this->menuRepository->findOneByAlias($alias);
+	/**
+	 * Load menu by alias
+	 * @param string $alias
+	 * @return Menu
+	 */
+	public function loadByAlias($alias): MenuInterface
+	{
+		$menu = $this->menuRepository->findOneByAlias($alias);
 
-        return $menu;
-    }
+		return $menu;
+	}
 
-    /**
-     * Remove a menu
-     *
-     * @param mixed $menu
-     */
-    public function remove($menu)
-    {
-        $menu = $this->menuRepository->remove($menu);
-    }
+	/**
+	 * Remove a menu
+	 * @param mixed $menu
+	 */
+	public function remove($menu)
+	{
+		$menu = $this->menuRepository->remove($menu);
+	}
 
-    /**
-     * Save a menu
-     *
-     * @param Menu $menu
-     */
-    public function save(MenuInterface $menu)
-    {
-        $this->menuRepository->save($menu);
-    }
+	/**
+	 * Save a menu
+	 * @param Menu $menu
+	 */
+	public function save(MenuInterface $menu)
+	{
+		$this->menuRepository->save($menu);
+	}
 
-    /**
-     * @return Menu[]
-     */
-    public function findAll()
-    {
-        return $this->menuRepository->findAll();
-    }
+	/**
+	 * @return Menu[]
+	 */
+	public function findAll(): array
+	{
+		return $this->menuRepository->findAll();
+	}
 
-    /**
-     * Get first level menu items
-     *
-     * @param Menu $menu
-     * @return MenuItems[]
-     */
-    public function getRootItems(MenuInterface $menu, $status)
-    {
-        return $this->getMenuItems($menu, static::ITEM_ROOT, $status);
-    }
+	/**
+	 * Get first level menu items
+	 * @param Menu $menu
+	 * @return MenuItems[]
+	 */
+	public function getRootItems(MenuInterface $menu, $status): array
+	{
+		return $this->getMenuItems($menu, static::ITEM_ROOT, $status);
+	}
 
-    /**
-     * Get enabled menu items
-     *
-     * @param Menu $menu
-     * @return MenuItems[]
-     */
-    public function getEnabledItems(MenuInterface $menu)
-    {
-        return $this->getMenuItems($menu, static::ITEM_ALL, static::STATUS_ENABLED);
-    }
+	/**
+	 * Get enabled menu items
+	 * @param Menu $menu
+	 * @return MenuItems[]
+	 */
+	public function getEnabledItems(MenuInterface $menu): array
+	{
+		return $this->getMenuItems($menu, static::ITEM_ALL, static::STATUS_ENABLED);
+	}
 
-    /**
-     * Get disabled menu items
-     *
-     * @param Menu $menu
-     * @return MenuItems[]
-     */
-    public function getDisabledItems(MenuInterface $menu)
-    {
-        return $this->getMenuItems($menu, static::ITEM_ALL, static::STATUS_DISABLED);
-    }
+	/**
+	 * Get disabled menu items
+	 * @param Menu $menu
+	 * @return MenuItems[]
+	 */
+	public function getDisabledItems(MenuInterface $menu)
+	{
+		return $this->getMenuItems($menu, static::ITEM_ALL, static::STATUS_DISABLED);
+	}
 
-    /**
-     * Get menu items
-     *
-     * @return MenuItem[]
-     */
-    public function getMenuItems(MenuInterface $menu, $root = self::ALL_ELEMENTS, $status = self::STATUS_ALL)
-    {
-        $menuItems = $menu->getMenuItems()->toArray();
+	/**
+	 * Get menu items
+	 * @return MenuItem[]
+	 */
+	public function getMenuItems(MenuInterface $menu, $root = self::ITEM_ALL, $status = self::STATUS_ALL)
+	{
+		$menuItems = $menu->getMenuItems()->toArray();
 
-        return array_filter($menuItems, function(MenuItemInterface $menuItem) use ($root, $status) {
-            // Check root parameter
-            if ($root === static::ITEM_ROOT && null !== $menuItem->getParent()
-             || $root === static::ITEM_CHILD && null === $menuItem->getParent()
-            ) {
-                return;
-            }
+		return array_filter($menuItems, function(MenuItemInterface $menuItem) use ($root, $status) {
+			// Check root parameter
+			if ($root === static::ITEM_ROOT && NULL !== $menuItem->getParent()
+				|| $root === static::ITEM_CHILD && NULL === $menuItem->getParent()
+			) {
+				return;
+			}
 
-            // Check status parameter
-            if ($status === static::STATUS_ENABLED && !$menuItem->getEnabled()
-             || $status === static::STATUS_DISABLED && $menuItem->getEnabled()
-            ) {
-                return;
-            }
+			// Check status parameter
+			if ($status === static::STATUS_ENABLED && !$menuItem->getEnabled()
+				|| $status === static::STATUS_DISABLED && $menuItem->getEnabled()
+			) {
+				return;
+			}
 
-            return $menuItem;
-        });
-    }
+			return $menuItem;
+		});
+	}
 
-    /**
-     * Update menu tree
-     *
-     * @param mixed $menu
-     * @param array $items
-     *
-     * @return bool
-     */
-    public function updateMenuTree($menu, $items, $parent=null)
-    {
-        $update = false;
+	/**
+	 * Update menu tree
+	 * @param mixed $menu
+	 * @param array $items
+	 * @return bool
+	 */
+	public function updateMenuTree($menu, $items, $parent = NULL)
+	{
+		$update = FALSE;
 
-        if(!($menu instanceof MenuInterface)) {
-            $menu = $this->load($menu);
-        }
+		if (!($menu instanceof MenuInterface)) {
+			$menu = $this->load($menu);
+		}
 
-        if(!empty($items) && $menu) {
+		if (!empty($items) && $menu) {
 
-            foreach ($items as $pos => $item) {
-                /** @var MenuItem $menuItem */
-                $menuItem = $this->menuItemRepository->findOneBy(array('id' => $item->id, 'menu' => $menu));
+			foreach ($items as $pos => $item) {
+				/** @var MenuItem $menuItem */
+				$menuItem = $this->menuItemRepository->findOneBy(['id' => $item->id, 'menu' => $menu]);
 
-                if($menuItem) {
-                    $menuItem
-                        ->setPosition($pos)
-                        ->setParent($parent)
-                    ;
+				if ($menuItem) {
+					$menuItem
+						->setPosition($pos)
+						->setParent($parent);
 
-                    $this->em->persist($menuItem);
-                }
+					$this->em->persist($menuItem);
+				}
 
-                if(isset($item->children) && !empty($item->children)) {
-                    $this->updateMenuTree($menu, $item->children, $menuItem);
-                }
-            }
+				if (isset($item->children) && !empty($item->children)) {
+					$this->updateMenuTree($menu, $item->children, $menuItem);
+				}
+			}
 
-            $this->em->flush();
+			$this->em->flush();
 
-            $update = true;
-        }
+			$update = TRUE;
+		}
 
-        return $update;
-    }
+		return $update;
+	}
 
 }
